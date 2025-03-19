f5afdb07a4aac734ef08a57d541e1dcfe04511df
UnBonWhisky <65244389+UnBonWhisky@users.noreply.github.com>
2025-03-19 17:14:29 +0100

    fix: banned account from myges

--- api-python/main.py
+++ api-python/main.py
@@ -33,7 +33,7 @@ def loop(minutes: int):
 
 @app.post("/api/calendar")
 async def get_calendar(user_creds: UserCreds, request: Request):
-    
+
     myges = MyGES_Informations()
     myges.basic_auth = user_creds.basic_auth
     myges.token = user_creds.token
@@ -43,14 +43,14 @@ async def get_calendar(user_creds: UserCreds, request: Request):
         if user_creds.basic_auth is None:
             raise HTTPException(status_code=400, detail="No basic_auth provided")
         login_informations = await myges.get_token(user_creds.basic_auth)
-        
+
         if login_informations.status_code != 302:
             raise HTTPException(status_code=401, detail="Invalid credentials")
-        
+
         else :
             user_creds.token = login_informations.token
             user_creds.expiry_date = login_informations.expiry_date
-    
+
     decode_auth = base64.b64decode(user_creds.basic_auth.encode()).decode().split(":")
 
     try :
@@ -58,25 +58,25 @@ async def get_calendar(user_creds: UserCreds, request: Request):
             await f.close()
     except FileNotFoundError:
         global_calendar = await myges.get_calendar_events()
-        
+
         await myges.create_calendar(global_calendar, basepath, decode_auth[0])
-        
+
     api_key = secrets.token_hex(16)
-    
+
     action_done, api_key = await myges.write_database(decode_auth[0], user_creds.basic_auth, api_key, user_creds.token, user_creds.expiry_date)
-    
-    
+
+
     return {
         "filename": f"{decode_auth[0]}",
         "api_key": api_key,
         "action": action_done
     }
-    
+
 @app.get("/calendar/feed/{filename}")
 async def return_calendar(filename: str, api_key: str = Query(None)):
     if api_key is None:
         raise HTTPException(status_code=400, detail="No api_key provided")
-    
+
     try :
         async with aiosqlite.connect('myges.db') as db:
             await db.execute("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, basic_auth TEXT, api_key TEXT, token TEXT, expiry_date INT, update_time TEXT)")
@@ -86,7 +86,7 @@ async def return_calendar(filename: str, api_key: str = Query(None)):
             response = await cursor.fetchone()
 
             await cursor.close()
-            
+
             if response is None:
                 raise HTTPException(status_code=403, detail="Invalid informations provided")
             else :
@@ -101,7 +101,7 @@ async def renew_calendar(request: CalendarSettings):
 
     if username is None or apikey is None:
         raise HTTPException(status_code=400, detail="No username and apikey provided")
-    
+
     await refresh_calendar(username, apikey)
     return "OK"
 
@@ -112,29 +112,29 @@ async def delete_calendar(request: CalendarSettings):
 
     if username is None or apikey is None:
         raise HTTPException(status_code=400, detail="No username and apikey provided")
-    
+
     async with aiosqlite.connect('myges.db') as db:
         await db.execute("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, basic_auth TEXT, api_key TEXT, token TEXT, expiry_date INT, update_time TEXT)")
         cursor = await db.cursor()
 
         await cursor.execute("SELECT username, api_key FROM users WHERE username = ? AND api_key = ?", (username, apikey,))
         response = await cursor.fetchone()
-        
+
         if response is None:
             raise HTTPException(status_code=404, detail="User not found")
-        
+
         await cursor.execute("DELETE FROM users WHERE username = ?", (username,))
         await db.commit()
         await cursor.close()
-    
+
     try :
         os.remove(f"{basepath}/{username}.ics")
     except FileNotFoundError:
         pass
-    
+
     return "OK"
 
-    
+
 
 @app.get('/api/token/get')
 async def extension_token_get(request: Request):
@@ -142,7 +142,7 @@ async def extension_token_get(request: Request):
     header = request.headers.get("Authorization")
     if header is None:
         raise HTTPException(status_code=400, detail="No Authorization header provided")
-    
+
     # Get the basic auth
     basic_auth = header.split(" ")[1]
     myges = MyGES_Informations()
@@ -151,7 +151,7 @@ async def extension_token_get(request: Request):
 
     if login_informations.status_code != 302:
         raise HTTPException(status_code=401, detail="Invalid credentials")
-    
+
     else :
 
         return {
@@ -177,27 +177,27 @@ async def refresh_calendar(username: str = None, apikey: str = None):
             responses = await cursor.fetchall()
 
         await cursor.close()
-        
+
         if responses is None or responses == []:
             return
-        
+
         for i, response in enumerate(responses) :
             myges = MyGES_Informations()
             if i % 5 == 0 :
                 await myges.setup_http_client()
             myges.basic_auth = response[1]
-            
+
             actual_time = int(datetime.datetime.now().timestamp())
-            
+
             if actual_time > response[3]:
                 login_informations = await myges.get_token(response[1])
-                
+
                 if login_informations.status_code != 302:
                     return
                 else :
                     myges.token = login_informations.token
                     myges.expiry_date = login_informations.expiry_date
-                    
+
                     cursor = await db.cursor()
                     await cursor.execute("UPDATE users SET token = ?, expiry_date = ? WHERE username = ?", (login_informations.token, login_informations.expiry_date, response[0],))
                     await db.commit()
@@ -207,9 +207,9 @@ async def refresh_calendar(username: str = None, apikey: str = None):
                 myges.expiry_date = response[3]
 
             global_calendar = await myges.get_calendar_events()
-            
+
             await myges.create_calendar(global_calendar, basepath, response[0])
-         
+
 async def start_server():
     config = uvicorn.Config(app, host="0.0.0.0", port=40500)
     server = uvicorn.Server(config)
